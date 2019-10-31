a31cf7d98f8c0153c7d6a38ca99d7f12a91dc222
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-10-31 19:12:45 +0000

    Initial commit.

---
+++ interfaces/project.clj
@@ -0,0 +1,4 @@
+(defproject realworld.burnup/interfaces "1.0"
+  :description "Component interfaces"
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :aot :all)
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject realworld.burnup/burnup "1.0"
+  :description "A Polylith workspace."
+  :plugins [[polylith/lein-polylith "0.2.2"]]
+  :polylith {:top-namespace "realworld.burnup"
+             :clojure-version "1.9.0"})
