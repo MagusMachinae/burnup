4d381405c40a73faea452111a36631211db18e55
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-11-01 11:41:25 +0000

    API deps added.

--- bases/rest-api/src/realworld/burnup/rest_api/api.clj
+++ bases/rest-api/src/realworld/burnup/rest_api/api.clj
@@ -9,5 +9,49 @@
             [ring.middleware.params :as pr]))
 
 ;; A stand alone base example. Change to the right type of base.
-(defn -main [& args]
-  (println "Hello world!"))
+(defroutes
+  (OPTIONS "/**"          []h/options)
+  (GET     "/api/health"  []h/health)
+  (POST    "/api/users"))
+
+(def ^:private app-routes
+  (routes
+    (-> private-routes
+        (wrap-routes m/wrap-authorization)
+        (wrap-routes m/wrap-auth-user))
+    (-> public-routes
+        (wrap-routes m/wrap-auth-user))
+    other-routes))
+
+(def app
+  (-> app-routes
+      logger/wrap-with-logger
+      kp/wrap-keyword-params
+      pr/wrap-params
+      mp/wrap-multipart-params
+      js/wrap-json-params
+      np/wrap-nested-params
+      m/wrap-exceptions
+      js/wrap-json-response
+      m/wrap-cors))
+
+(defn init []
+  (try
+    (log/init)
+    (let [db (database/db)]
+      (if (database/db-exists?)
+        (if (database/valid-schema? db)
+          (log/info "Database schema is valid.")
+          (do
+            (log/warn "Please fix database schema and restart")
+            (System/exit 1)))
+        (do
+          (log/info "Generating database.")
+          (database/generate-db db)
+          (log/info "Database generated."))))
+    (log/info "Initialized server.")
+    (catch Exception e
+      (log/error e "Could not start server."))))
+
+(defn destroy []
+  (log/info "Destroyed server."))
