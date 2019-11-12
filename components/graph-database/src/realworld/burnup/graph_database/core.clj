635a49646611c5859f91fcfa0ffe17798349e70a
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-11-12 23:57:30 +0000

    Working on db helper functions

--- components/graph-database/src/realworld/burnup/graph_database/core.clj
+++ components/graph-database/src/realworld/burnup/graph_database/core.clj
@@ -1,7 +1,34 @@
 (ns realworld.burnup.graph-database.core
-  (:require [datomic.api :as d]))
+  (:import datomic.Util)
+  (:require [clojure.java.io :as io]
+            [environ.core :refer env]))
 
 
 ;; add your functions here...
-(defn add-two [x]
-  (+ 2 x))
+(defn- db-path []
+  (if (contains? env :database)
+    (env :database)
+    "database.edn"))
+
+(defn- scratch-conn)
+
+(defn read-all
+  "Read all forms in f, where f is any resourced that
+can be opened by io/reader"
+  [f]
+  (Util/readAll (io/reader f)))
+
+(defn transact-all
+  "Load and run all transactions from f, where f is
+any resource that can be opened by io/reader"
+  [conn f]
+  (loop [n 0
+         [tx & more] (read-all f)]
+    (if tx
+      (recur (+ n (count (:tx-data @(d/transact conn tx))))
+             more)
+      {:datoms n})))
+
+((defn db
+  [path]
+  {}))
--- components/graph-database/src/realworld/burnup/graph_database/schema.clj
+++ components/graph-database/src/realworld/burnup/graph_database/schema.clj
@@ -1,6 +1,8 @@
 (ns realworld.burnup.schema
-  (:require [realworld.burnup.log.interface :as log]))
+  (:require [realworld.burnup.log.interface :as log]
+            [datomic.api :as d]))
 
-(def contractor)  
+(def contractor
+  )
 
-(def )
+(def)
