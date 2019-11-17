0268279c48416c63b5c25b72cf87c30d02526013
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-11-17 00:48:16 +0000

    Inited user.spec

---
+++ components/user/src/realworld/burnup/user/spec.clj
@@ -0,0 +1,7 @@
+(ns realworld.burnup.user.spec
+  (:require [realworld.burnup.spec.interface :as spec]
+            [spec-spec-tools.core :as st]
+            [spec-tools.data-spec :as ds]))
+
+(def id
+  (st/spec))
