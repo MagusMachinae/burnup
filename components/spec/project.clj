3781480192112e64fae2b0056189b1840d5e2eba
MagusMachinae <Edward.Hughes1911@gmail.com>
2019-11-17 00:40:27 +0000

    Added spec deps

--- components/spec/project.clj
+++ components/spec/project.clj
@@ -1,5 +1,6 @@
 (defproject realworld.burnup/spec "0.1"
   :description "A spec component."
   :dependencies [[realworld.burnup/interfaces "1.0"]
-                 [org.clojure/clojure "1.9.0"]]
+                 [org.clojure/clojure "1.9.0"]
+                 [metosin/spec-tools "0.6.1"]]
   :aot :all)
